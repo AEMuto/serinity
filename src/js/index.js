3efb3de71713d9c70557db6556cfef28669089d4
AEMuto <antoine.marseaud@gmail.com>
2025-03-11 23:03:55 +0100

    LANDING_PAGE: progressing on responsive media queries

--- src/js/index.js
+++ src/js/index.js
@@ -8,17 +8,10 @@ console.log("Serinity Theme loaded");
 
 // Add your JS functionality
 document.addEventListener("DOMContentLoaded", function () {
-  const sliders = initCardSliders();
+  const methods_cards = initCardSliders();
   // Initialize testimonial carousels with options
   const testimonialsCarousels = initCarousels(".serinity-testimonies", ".serinity-testimony", {
     direction: "left", // "left" or "right"
-    speed: 0.5, // Pixels per frame
-    randomOpacity: false, // Random initial opacity
-    timeBasedAnimation: false, // Enable time-based opacity and blur
-  });
-
-  const questionsCarousels = initCarousels(".serinity-questions", ".serinity-question", {
-    direction: "right", // "left" or "right"
     speed: 0.5, // Pixels per frame
     randomOpacity: false, // Random initial opacity
     timeBasedAnimation: false, // Enable time-based opacity and blur
