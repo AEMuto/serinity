7879a3817f20d8677ffe343d9e97a5b48269c9b5
AEMuto <antoine.marseaud@gmail.com>
2025-03-18 19:41:31 +0100

    LANDING_PAGE: refactor media queries to use variables for breakpoints and enhance responsiveness

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,27 @@
 // Import main SCSS file
 import "../scss/serinity.scss";
-import { initCardSliders } from "./card-slider";
-import { initCarousels } from "./testimony-carousel";
+import { initCardSliders } from "./serinity-slider";
+import { initCarousels } from "./serinity-carousel";
 
 // Theme JavaScript goes here
 console.log("Serinity Theme loaded");
 
 // Add your JS functionality
 document.addEventListener("DOMContentLoaded", function () {
+  // Remove admin bar height from root element
   const hasAdminBar = !!document.querySelector("#wpadminbar");
-  if (!hasAdminBar) document.documentElement.style.setProperty('--wp-admin--admin-bar--height', '0px');
+  if (!hasAdminBar)
+    document.documentElement.style.setProperty("--wp-admin--admin-bar--height", "0px");
+
+  // Responisve Nav menu toggle
+  const hamburger = document.querySelector('.serinity-hamburger-menu');
+  const navMenu = document.querySelector('.serinity-header-navigation-menu');
+  hamburger.addEventListener('click', function() {
+    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+  });
+
+  // Expand plans
   const expandPlansBtn = document.querySelector("#expand-plans");
   const plans = document.querySelector("#plans");
 
@@ -20,17 +32,18 @@ document.addEventListener("DOMContentLoaded", function () {
       const offset = plans.offsetTop - 100;
       window.scrollTo({
         top: offset,
-        behavior: "smooth"
+        behavior: "smooth",
       });
     }
   });
 
+  // Initialize card sliders
   const methods_cards = initCardSliders();
 
   // Initialize testimonial carousels with options
   const testimonialsCarousels = initCarousels(".serinity-testimonies", ".serinity-testimony", {
     direction: "left", // "left" or "right"
-    speed: .5, // Pixels per frame
+    speed: 0.5, // Pixels per frame
     mobileTitle: "Témoignages", // Title for mobile carousel
     debug: false,
   });
