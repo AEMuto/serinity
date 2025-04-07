16f07a8482ab1b7b601b38034a8aa0e43e478763
AEMuto <antoine.marseaud@gmail.com>
2025-04-07 19:14:46 +0200

    REFINE: enhance link styles in paragraphs and update image URLs in 404 template

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // Import main SCSS file
 import "../scss/serinity.scss";
-import { initCardSliders } from "./serinity-slider";
 import { initCarousels } from "./serinity-carousel";
 
 // Theme JavaScript goes here
@@ -15,30 +14,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Responisve Nav menu toggle
   const hamburger = document.querySelector('.serinity-hamburger-menu');
-  const navMenu = document.querySelector('.serinity-header-navigation-menu');
   hamburger.addEventListener('click', function() {
     const expanded = this.getAttribute('aria-expanded') === 'true' || false;
     this.setAttribute('aria-expanded', !expanded);
   });
-
-  // Expand plans
-  const expandPlansBtn = document.querySelector("#expand-plans");
-  const plans = document.querySelector("#plans");
-
-  expandPlansBtn.addEventListener("click", (e) => {
-    plans.classList.toggle("open");
-    if (plans.classList.contains("open")) {
-      // scroll to anchor #aurea
-      const offset = plans.offsetTop - 100;
-      window.scrollTo({
-        top: offset,
-        behavior: "smooth",
-      });
-    }
-  });
-
-  // Initialize card sliders
-  const methods_cards = initCardSliders();
 
   // Initialize testimonial carousels with options
   const testimonialsCarousels = initCarousels(".serinity-testimonies", ".serinity-testimony", {
